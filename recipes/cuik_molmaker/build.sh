b8c67cbc53021075afdcf8d2e7c41dcc2f5e6946
sveccham <sveccham@nvidia.com>
2025-11-03 12:16:10 -0800

    cuik-molmaker v0.2

--- recipes/cuik_molmaker/build.sh
+++ recipes/cuik_molmaker/build.sh
@@ -1,11 +1,7 @@
 # Get Python version from conda-build environment (e.g., 3.11, 3.12, 3.13)
 export PYTHON_VERSION=${PY_VER}
 
-# Validate Torch version
-if [[ ! $TORCH_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Error: Torch version must be in the format: year.minor.patch 2.6.0"
-    exit 1
-fi
+CXX11_ABI="ON"
 
 # Validate RDKit version format (year.minor.patch)
 if [[ ! $RDKIT_VERSION =~ ^[0-9]{4}\.[0-9]+\.[0-9]+$ ]]; then
@@ -26,23 +22,22 @@ echo "${BUILD_PREFIX}"
 set -x
 
 # Hack for RDkit expecting things in python3.9 folder
-ln -s $CONDA_PREFIX/lib/python$PYTHON_VERSION $CONDA_PREFIX/lib/python3.9
-ln -s $CONDA_PREFIX/include/python$PYTHON_VERSION $CONDA_PREFIX/include/python3.9
-
+# ln -s $CONDA_PREFIX/lib/python$PYTHON_VERSION $CONDA_PREFIX/lib/python3.9
+# ln -s $CONDA_PREFIX/include/python$PYTHON_VERSION $CONDA_PREFIX/include/python3.9
 
 # Build C++ extension
-TORCH_VERSION=${TORCH_VERSION} RDKIT_VERSION=${RDKIT_VERSION}   PYTHON_VERSION=${PYTHON_VERSION} $PYTHON setup.py build_ext --inplace
+RDKIT_VERSION=${RDKIT_VERSION}  PYTHON_VERSION=${PYTHON_VERSION} CUIKMOLMAKER_CXX11_ABI=${CXX11_ABI} $PYTHON setup.py build_ext --inplace
 
 # Set the wheel directory
-export WHL_DIR=cm_py${PYTHON_VERSION}_rdkit-${RDKIT_VERSION}_torch-${TORCH_VERSION}_dist
+export WHL_DIR=cm_py${PYTHON_VERSION}_rdkit-${RDKIT_VERSION}_dist
 
 # Build wheel
-TORCH_VERSION=${TORCH_VERSION} RDKIT_VERSION=${RDKIT_VERSION}   PYTHON_VERSION=${PYTHON_VERSION} $PYTHON setup.py bdist_wheel --dist-dir ${WHL_DIR}
+RDKIT_VERSION=${RDKIT_VERSION}   PYTHON_VERSION=${PYTHON_VERSION} CUIKMOLMAKER_CXX11_ABI=${CXX11_ABI} $PYTHON setup.py bdist_wheel --dist-dir ${WHL_DIR}
 
 # Install the package from the dist directory
 $PYTHON -m pip install --no-deps --no-build-isolation --prefix=$PREFIX ${WHL_DIR}/*.whl
 
 echo "PREFIX:"
 echo "${PREFIX}"
-$PYTHON -c "import cuik_molmaker; print(cuik_molmaker.__file__)"
+# $PYTHON -c "import cuik_molmaker; print(cuik_molmaker.__file__)"
 set +x
